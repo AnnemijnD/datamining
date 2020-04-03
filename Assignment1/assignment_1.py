a69456e3daa0a589745e747a7b4c090e27210808
Sanne Donker <s.a.m.donker@hotmail.com>
2020-04-03 15:23:45 +0200

    new column names

--- Assignment1/assignment_1.py
+++ Assignment1/assignment_1.py
@@ -2,4 +2,34 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_excel('data/ODI-2020.xlsx')
-print(df)
+# print(df.head())
+
+column_dict = {'What programme are you in?': 'programme',
+            'Have you taken a course on machine learning?': 'machinelearning',
+            'Have you taken a course on information retrieval?': 'informationretrieval',
+            'Have you taken a course on statistics?': 'statistics',
+            'Have you taken a course on databases?': 'databases',
+            'What is your gender?': 'gender',
+            'Chocolate makes you.....': 'chocolate',
+            'When is your birthday (date)?': 'birthday',
+            'Number of neighbors sitting around you?': 'neighbors',
+            'Did you stand up?': 'stand',
+            'What is your stress level (0-100)?': 'stresslevel',
+            'You can get 100 euros if you win a local DM competition, or we don’t hold any competitions and I give everyone some money (not the same amount!). How much do you think you would deserve then? ': 'money',
+            'Give a random number': 'randomnumber',
+            'Time you went to be Yesterday': 'bedtime',
+            'What makes a good day for you (1)?': 'goodday1',
+            'What makes a good day for you (2)?': 'goodday2'}
+
+df_new = df.rename(columns=column_dict)
+
+valid_entries = 0
+for index, row in df_new.iterrows():
+    if type(row['money']) == str:
+        print(row['money'])
+    # if type(row['money']) == int or type(row['money']) == float:
+    else:
+        if row['money'] >= 0 and row['money'] <= 100:
+            # print(row['money'])
+            valid_entries += 1
+print(valid_entries)
