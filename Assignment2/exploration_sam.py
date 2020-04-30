2d33eab8a601dd9b276a8621116a01d8ce3792ae
samverh <43989279+samverh@users.noreply.github.com>
2020-04-30 15:27:20 +0200

    plotjes

---
+++ Assignment2/exploration_sam.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from preprocessing import drop_cols
+
+sns.set()
+sns.set_color_codes("pastel")
+
+
+def plot(var, data):
+    """
+    Makes a plot for the booked fraction of a variable of the training data set.
+    """
+    var_names = {"promotion_flag": "Promotion", "random_bool": "Random",\
+                "prop_review_score": "Review score", "prop_starrating": "Star rating",\
+                "prop_brand_bool": "Large brand", "prop_location_score1": "Location score 1",\
+                "prop_location_score2": "Location score 1"}
+
+    sns.barplot(x=var, y="booking_bool", data=data, color="b",\
+                capsize=.1, errwidth=1.1).tick_params(labelsize=18)
+
+    # plot style properties
+    ax = plt.gca()
+
+    for ax in plt.gcf().axes:
+        x = ax.get_xlabel()
+        y = ax.get_ylabel()
+        ax.set_xlabel(x, fontsize=20)
+        ax.set_ylabel(y, fontsize=20)
+
+    plt.title("Bookings for variable " + str(var), fontsize=22)
+    plt.ylabel("Fraction")
+    plt.xlabel(var_names[var])
+    t = ax.title
+    t.set_position([.5, 1.05])
+    # plt.ylim([0, 1])
+    plt.subplots_adjust(bottom=.15, left=.15)
+    plt.savefig("results/booking_" + str(var) + ".png", bbox_inches="tight")
+
+    # plt.show()
+
+def plot_2(var, data):
+    """
+    Makes a plot for the booked fraction of a variable of the training data set.
+    """
+    var_names = {"promotion_flag": "Promotion", "random_bool": "Random",\
+                "prop_review_score": "Review score", "prop_starrating": "Star rating",\
+                "prop_brand_bool": "Large brand", "prop_location_score1": "Location score 1",\
+                "prop_location_score2": "Location score 1"}
+
+    sns.barplot(x=var, y="booking_bool", hue="comp1_rate", data=data, color="b",\
+                capsize=.1, errwidth=1.1).tick_params(labelsize=18)
+
+    # plot style properties
+    ax = plt.gca()
+
+    for ax in plt.gcf().axes:
+        x = ax.get_xlabel()
+        y = ax.get_ylabel()
+        ax.set_xlabel(x, fontsize=20)
+        ax.set_ylabel(y, fontsize=20)
+
+    plt.title("Bookings for variable " + str(var), fontsize=22)
+    plt.ylabel("Fraction")
+    plt.xlabel(var_names[var])
+    t = ax.title
+    t.set_position([.5, 1.05])
+    # plt.ylim([0, 1])
+    plt.subplots_adjust(bottom=.15, left=.15)
+    plt.savefig("results/booking_" + str(var) + ".png", bbox_inches="tight")
+
+    # plt.show()
+
+if __name__ == "__main__":
+
+    # load data
+    df_train = pd.read_csv("data/training_set_VU_DM.csv")
+    # df_train = pd.read_csv("data/training_short.csv")
+
+    # drop variables
+    data = drop_cols(df_train)
+
+    # print(data.groupby([data["random_bool"], data["promotion_flag"]])["booking_bool"].count())
+    # print(data.groupby([data["comp1_rate"], data["comp2_rate"]])["booking_bool"].count())
+
+    variables = ["promotion_flag", "random_bool", "prop_review_score", "prop_starrating", "prop_brand_bool"]
+
+    # make plots for each variable
+    # for var in variables:
+    #     plot(var, data)
+
+    sns.countplot("srch_id", data=data)
+    plt.show()
+
+# # filter hotels that were booked
+# booked = data[data["booking_bool"] == 1]
+#
+# # countplot for countries that customers travel from
+# sns.countplot('visitor_location_country_id', data=booked.sort_values(by=['visitor_location_country_id']),palette="Set3")
+# plt.show()
+# sns.countplot('visitor_location_country_id', data=booked, palette="Set3")
+# plt.show()
+#
+# # countplot for countries that customers travel to
+# sns.countplot('prop_country_id', data=booked.sort_values(by=['prop_country_id']), palette="Set3")
+# plt.show()
+# sns.countplot('prop_country_id', data=booked, palette="Set3")
+# plt.show()
--- Assignment2/preprocessing.py
+++ Assignment2/preprocessing.py
@@ -4,61 +4,107 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import SelectKBest, f_classif
+from IPython.display import display
+import time
+from tqdm import tqdm
 
 
-# load data
-df_train = pd.read_csv("data/training_set_VU_DM.csv")
-# df_test = pd.read_csv("data/test_set_VU_DM.csv")
+# to view dataset
+def display_df(df):
+    """
+    Display full dataframe in terminal.
+    """
+    with pd.option_context("display.max_rows", 1000, "display.max_columns", 100):
+        display(df)
 
-print(df_train.head(10))
-# print(df_test.head(10))
 
-# datasets = [df_train, df_test]
-# for data in datasets:
-data = df_train.sample(n=1000)
-# get latex table for summaries of categorical and numerical data
-numeric_columns = data.select_dtypes(include=['int64', 'float64']).columns.tolist()
-categorical_columns = data.select_dtypes(include=["object", "category"]).columns.tolist()
-print('numeric columns: ' + str(numeric_columns))
-print(round(data[numeric_columns].describe(),2)) # MEMORYERROR
-print('categorical columns: ' + str(categorical_columns))
-print(data[categorical_columns].describe())
-# quit()
-# drop categorical
-data.drop(categorical_columns, axis=1, inplace=True)
+def shorten():
 
-print("MISSING VALS BEFORE")
-for col in data.columns.values:
-    if data[col].isnull().any():
-        print(f"Missing values in {col}")
+    # load data
+    df_train = pd.read_csv("data/training_set_VU_DM.csv")
+    df_test = pd.read_csv("data/test_set_VU_DM.csv")
 
-for var in numeric_columns:
-    data[var].fillna(data.groupby("prop_starrating")[var].transform("mean"), inplace=True)
+    print(df_train.head(10))
+    print(df_test.head(10))
 
-to_drop = []
-print("MISSING VALS AFTER")
-for col in data.columns.values:
-    if data[col].isnull().any():
-        to_drop.append(col)
-        
-data.drop(to_drop, axis=1, inplace=True)
+    df_train.sample(n=1000).to_csv("data/training_short.csv", index=False)
+    df_test.sample(n=1000).to_csv("data/test_short.csv", index=False)
 
-# scale numeric
-scaler = StandardScaler()
 
-for var in numeric_columns:
-    data[var] = data[var].astype("float64")
-    data[var] = scaler.fit_transform(data[var].values.reshape(-1, 1))
+def test_bools():
+    """
+    There are no booked hotels that were not clicked on.
+    """
 
-# importance
-target = data['position'].values
-select_features = data.columns.values
+    # load data
+    df_train = pd.read_csv("data/training_set_VU_DM.csv")
 
-selector = SelectKBest(f_classif, len(select_features))
-selector.fit(data, target)
-scores = -np.log10(selector.pvalues_)
-indices = np.argsort(scores)[::-1]
+    for index, row in tqdm(df_train.iterrows(), total=len(df_train)):
+        if row["click_bool"] == 0 and row["booking_bool"] == 1:
+            print("trick does not work")
+            quit()
+    print("trick works")
 
-print('Features importance:')
-for i in range(len(scores)):
-    print('%.2f %s' % (scores[indices[i]], select_features[indices[i]]))
+
+def drop_cols(df):
+    """
+    Drop variables that are not of interest.
+    """
+    uninteresting = ["srch_adults_count", "srch_children_count", "srch_room_count", "date_time", "site_id"]
+    df.drop(uninteresting, axis=1, inplace=True)
+
+    return df
+
+
+if __name__ == "__main__":
+
+    # load data
+    df_train = pd.read_csv("data/training_short.csv")
+    # df_test = pd.read_csv("data/test_short.csv")
+
+    # summaries of categorical and numerical data
+    numeric_columns = data.select_dtypes(include=['int64', 'float64']).columns.tolist()
+    categorical_columns = data.select_dtypes(include=["object", "category"]).columns.tolist()
+    print('numeric columns: ' + str(numeric_columns))
+    display_df(round(data[numeric_columns].describe(),2)) # MEMORYERROR
+    print('categorical columns: ' + str(categorical_columns))
+    print(data[categorical_columns].describe())
+    quit()
+    # drop categorical
+    data.drop(categorical_columns, axis=1, inplace=True)
+
+    print("MISSING VALS BEFORE")
+    for col in data.columns.values:
+        if data[col].isnull().any():
+            print(f"Missing values in {col}")
+
+    for var in numeric_columns:
+        data[var].fillna(data.groupby("prop_starrating")[var].transform("mean"), inplace=True)
+
+    to_drop = []
+    print("MISSING VALS AFTER")
+    for col in data.columns.values:
+        if data[col].isnull().any():
+            to_drop.append(col)
+
+    data.drop(to_drop, axis=1, inplace=True)
+
+    # scale numeric
+    scaler = StandardScaler()
+
+    for var in numeric_columns:
+        data[var] = data[var].astype("float64")
+        data[var] = scaler.fit_transform(data[var].values.reshape(-1, 1))
+
+    # importance
+    target = data['position'].values
+    select_features = data.columns.values
+
+    selector = SelectKBest(f_classif, len(select_features))
+    selector.fit(data, target)
+    scores = -np.log10(selector.pvalues_)
+    indices = np.argsort(scores)[::-1]
+
+    print('Features importance:')
+    for i in range(len(scores)):
+        print('%.2f %s' % (scores[indices[i]], select_features[indices[i]]))
